90e338db8c17cecf387db5f88426199b339a3ef2
Rafael Gonçalves <rafaelgonsalveslima21@gmail.com>
2021-07-20 20:12:37 -0300

    funcionamento refatorado

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,20 +1,46 @@
-export default function initFuncionamento() {
-const funcionamento = document.querySelector('[data-semana]')
-const diasSemana = funcionamento.dataset.semana.split(',').map(Number);
-const horarioSemana = funcionamento.dataset.horario.split(',').map(Number);
+export default class Funcionamento {
+  constructor(funcionamento, activeClass) {
+    this.funcionamento = document.querySelector(funcionamento);
+    this.activeClass = activeClass;
+  }
 
+  dadosFuncionamento() {
+    this.diasSemana = this.funcionamento.dataset.semana.split(",").map(Number);
+    this.horarioSemana = this.funcionamento.dataset.horario
+      .split(",")
+      .map(Number);
+  }
 
-const dataAgora = new Date()
-const diaAgora = dataAgora.getDay()
-const horarioAgora = dataAgora.getHours()
+  dadosAgora() {
+    this.dataAgora = new Date();
+    this.diaAgora = this.dataAgora.getDay();
+    this.horarioAgora = this.dataAgora.getUTCHours() - 3;
+  }
 
-const semanaAberta = diasSemana.indexOf(diaAgora) !== -1
+  estaAberto() {
+    this.semanaAberta = this.diasSemana.indexOf(this.diaAgora) !== -1;
 
-const horarioAberto = (horarioAgora >= horarioSemana[0] && horarioAgora < horarioSemana[1])
+    this.horarioAberto =
+      this.horarioAgora >= this.horarioSemana[0] &&
+      this.horarioAgora < this.horarioSemana[1];
 
-if(semanaAberta && horarioAberto) {
-  funcionamento.classList.add('aberto')
-}
+    return this.semanaAberta && this.horarioAberto;
+  }
+
+  ativaAberto() {
+    if (this.estaAberto()) {
+      this.funcionamento.classList.add(this.activeClass);
+    }
+  }
+
+  init() {
+    if (this.funcionamento) {
+      this.dadosFuncionamento();
+      this.dadosAgora();
+      this.ativaAberto();
+    }
+    return this;
+  }
 }
 
 // const agora = new Date
@@ -31,4 +57,3 @@ if(semanaAberta && horarioAberto) {
 // const diasfuturo = transformaDias(futuro.getTime())
 
 // console.log(diasfuturo - diasAgora)
-
